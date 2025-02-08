dedf5c1569ecd0920b8424f37aabc6278da3983f
XxKillerDemonxX <adamtan443@gmail.com>
2025-02-07 18:41:41 -0800

    some more stuff

--- layers.py
+++ layers.py
@@ -3,6 +3,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+# NOTES TO SELF:
+# add dropout layers wherever needed
+# make sure everything is on the right device layer, ie. need device = device...
+
 
 # -----MULTI HEAD ATTENTION LAYER-----
 # embed_dim: embedding dimension of the input tokens
@@ -33,6 +37,10 @@ class MultiHeadAttention(nn.Module):
         scaling_factor = q.size(self.head_dim) ** 0.5
         attn_score = attn_score/scaling_factor
 
+        # masking
+        tril = torch.tril((torch.ones(context_length, context_length)))
+        attn_score = attn_score.masked_fill(tril==0, float('-inf'))
+
         # softmax
         attn_weights = F.softmax(attn_score, dim = -1)
 
@@ -47,4 +55,31 @@ class MultiHeadAttention(nn.Module):
         return list(self.parameters())
 
 
-        
+#-----LAYER NORM-----
+class LayerNorm(nn.Module):
+    def __init__(self, embed_dim):
+        super(LayerNorm, self).__init__()
+        self.weights = nn.Parameter(torch.ones(embed_dim))
+        self.bias = nn.Parameter(torch.zeros(embed_dim))
+    def forward(self, x):
+        xmean = x.mean(1, keepdim=True)
+        xvar = x.var(1, keepdim=True)
+        y = (x - xmean) / torch.sqrt(xvar)
+        self.out = self.weights * y + self.bias
+        return self.out
+    def param(self):
+        return list(self.parameters())
+    
+#-----MLP-----
+class FeedForward(nn.Module):
+    def __init__(self, embed_dim):
+        super(FeedForward, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(embed_dim, 4 * embed_dim),
+            nn.reLU(),
+            nn.Linear(4 * embed_dim, embed_dim),
+        )
+    def forward(self, x):
+        return self.layers(x)
+    def param(self):
+        return list(self.parameters())
--- transformer.py
+++ transformer.py
@@ -1,9 +1,14 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 
 
-
-
+class block(nn.Module):
+    def __init__(self):
+        super(block, self).__init__()
+    def forward(self, x):
+        return
 
 
 
