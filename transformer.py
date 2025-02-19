0fffa729b6f58b13a7b0c10c46c0d944334997b9
XxKillerDemonxX <149117791+XxKillerDemonxX@users.noreply.github.com>
2025-02-19 14:51:46 -0800

    Update transformer.py

--- transformer.py
+++ transformer.py
@@ -1,12 +1,13 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from layers import MultiHeadAttention, LayerNorm, FeedForward
+from layers import MultiHeadAttention, LayerNorm, FeedForward, EmbeddingLayer
 
 
 embed_dim = 64
 num_head = 8
-
+vocab_length = 100
+context_length = 100
 
 class TransformerBlock(nn.Module):
     def __init__(self):
@@ -20,6 +21,19 @@ class TransformerBlock(nn.Module):
         x = x + self.ff.forward(self.ln2.forward(x))
         return x
 
-
+class Transformer(nn.Module):
+    def __init__(self):
+        super(Transformer, self).__init__()
+        # embedding: has an embedding for each token
+        embedding = EmbeddingLayer(vocab_length, embed_dim)
+        # positional embedding: has an embedding for token position
+        positionEmbedding = EmbeddingLayer(context_length, embed_dim)
+        # transformer blocks
+        self.blocks = nn.ModuleList([TransformerBlock() for _ in range(3)])
+        # final linear layer to output of shape (batch_size, context_length, vocab_length)
+        # when generating output, we would take the last index in context_length and use it's vocab_length to predict the next token
+        outputLinear = nn.Linear(embed_dim, vocab_length)
+    def forward(self, x):
+        return
 
 
